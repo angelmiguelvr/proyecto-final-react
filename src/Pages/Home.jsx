c4f3b6b9d98199855839b419ade9c4e7c3706985
angel <miguelangelvaldezramirez@gmail.com>
2023-01-23 23:17:54 -0600

    carousel

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,10 @@
 import {getDocs, collection} from "firebase/firestore/lite"
 import {FirebaseBD} from "../firebase/config";
 import {useEffect, useState} from "react";
+import {AdvancedCarousel} from "../components/Carousel/Carousel";
 import {Product} from "../components/Product/Product";
+
+
 export const Home = () => {
 	const [tools, setTools] = useState([]);
 
@@ -21,12 +24,16 @@ export const Home = () => {
 
 	return (
 		<main>
+
 			<div className="container">
+
+				<AdvancedCarousel/>
+
 				<h1>Herramientas</h1>
 
 				<ul style={{marginBottom: 0}}>
-					{tools.map(({id, nombre, imagen}) => (
-						<Product key={id} id={id} nombre={nombre} imagen={imagen}/>
+					{tools.map(({id, nombre, imagen, precio}) => (
+						<Product key={id} id={id} nombre={nombre} precio= {precio} imagen={imagen}/>
 					))}
 				</ul>
 			</div>
